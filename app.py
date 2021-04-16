3663545eb7c65e2071217fb6d0bf202ce1c44ad8
NicoPauwels <nicopauwels@MacBook-Pro-van-Nico.local>
2021-04-16 21:21:04 +0200

    App routing updated; session user; removed flash messaging

--- app.py
+++ app.py
@@ -18,12 +18,6 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_activities")
-def get_activities():
-    activities = mongo.db.activities.find()
-    return render_template("activities.html", activities=activities)
-
-
 @app.route("/register", methods=["GET","POST"])
 def register():
     if request.method == "POST":
@@ -43,18 +37,15 @@ def register():
             return redirect(url_for("register"))
 
         register = {
-            "username": request.form.get("username").lower(),
             "email": request.form.get("email").lower(),
             "password": generate_password_hash(request.form.get("password1"))
         }
         mongo.db.users.insert_one(register)
 
-        # put user in session cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Registration Succesful!")
-        return redirect(url_for(
-            "profile", username=session["user"]))
-    return render_template("register.html")
+        # put user in session cookie AND GO TO ACTIVITY PAGE TODO!
+        session["user"] = request.form.get("email").lower()
+        return redirect(url_for("get_activities"))
+    return render_template("register.html", isRegister=True)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -71,39 +62,38 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password1")):
                     session["user"] = request.form.get("email").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("email")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+                    return redirect(url_for("get_activities"))
             else:
                 # invalid password
-                flash("You have entered incorrect login credentials.")
                 return redirect(url_for("login"))
 
         else:
             # username does not exist
-            flash("You have entered incorrect login credentials.")
             return redirect(url_for("login"))
 
-    return render_template("login.html")
+    return render_template("login.html", isLogin=True)
 
 
-@app.route("/profile/<username>", methods=["GET", "POST"])
-def profile(username):
-    # get the session username
-    username = mongo.db.users.find_one(
+
+@app.route("/get_activities")
+def get_activities():
+    activities = mongo.db.activities.find()
+    return render_template("activities.html", activities=activities, isActivities=True)
+
+
+@app.route("/profile/<email>", methods=["GET", "POST"])
+def profile(email):
+    # get the session user by registered email address
+    email = mongo.db.users.find_one(
         {"email": session["user"]})["email"]
+    )
     
-    if session["user"]:
-        return render_template("profile.html", username=username)
-
-    return redirect(url_for("login"))
+    return render_template("profile.html", email=email, isProfile=True)
 
 
 @app.route("/logout")
 def logout():
     # remove user from session cookie
-    flash("You have been logged out.")
     session.pop("user")
     return redirect(url_for("login"))
 
