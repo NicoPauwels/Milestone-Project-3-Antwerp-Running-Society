6025fa1f685d55af21d4200af03b64b4d551e2ba
NicoPauwels <nico_pauwels@hotmail.com>
2021-04-19 16:18:21 +0200

    Initial js map config

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,9 @@
-var token ="pk.eyJ1IjoibmNwd2xzIiwiYSI6ImNrbmx1YTR1ejBha2gybmt4cDVrcDN6OWgifQ.wmt7OvrQdw7yqsgvK5h_xg"; // replace with your Mapbox API Access token. Create a Mapbox account and find it on https://account.mapbox.com/
+var mymap = L.map('mapid').setView([51.208, 4.425], 13);
 
-var map = L.map('map').setView([38.912753, -77.032194], 15);
-L.marker([38.912753, -77.032194])
-    .bindPopup("Hello <b>Leaflet GL</b>!<br>Whoa, it works!")
-    .addTo(map)
-    .openPopup();
-var gl = L.mapboxGL({
-    accessToken: token,
-    style: 'mapbox://styles/mapbox/bright-v8'
-}).addTo(map);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibmNwd2xzIiwiYSI6ImNrbmx1YTR1ejBha2gybmt4cDVrcDN6OWgifQ.wmt7OvrQdw7yqsgvK5h_xg'
+}).addTo(mymap);
