7ba4007a5f4eb98bcd67ab72f11b23008fb92e13
NicoPauwels <nico_pauwels@hotmail.com>
2021-05-07 23:16:44 +0200

    zoomControl position and checkDate functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
+// leaflet map
+
 var mymap = L.map('mapid').setView([51.208, 4.425], 13);
+
+mymap.zoomControl.setPosition('topright');
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 18,
@@ -7,6 +11,16 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoibmNwd2xzIiwiYSI6ImNrbmx1YTR1ejBha2gybmt4cDVrcDN6OWgifQ.wmt7OvrQdw7yqsgvK5h_xg'
 }).addTo(mymap);
+
+// check whether inputdate is bigger than now
+
+function checkDate() {
+  var inputdate = new Date(document.getElementById('date').value);
+  var today = new Date();
+  if(inputdate.getDate() < today.getDate()) {
+    console.log("Input date is in the past.")
+  }
+}
 
 // add run form distance slider
 
