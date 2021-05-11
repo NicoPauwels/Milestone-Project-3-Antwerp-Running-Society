b954677ed05be862a08298989e9aec4f157a1db9
NicoPauwels <nico_pauwels@hotmail.com>
2021-05-11 14:07:48 +0200

    user level determination

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ mongo = PyMongo(app)
 
 timestamp = datetime.datetime.now()
 createdon = timestamp.strftime("%d/%m/%Y, %H:%M:%S")
+createdondate = timestamp.strftime("%d/%m/%Y")
 
 
 
@@ -46,13 +47,15 @@ def register():
         register = {
             "email": request.form.get("email").lower(),
             "password": generate_password_hash(request.form.get("password1")),
-            "membersince": createdon
+            "membersince": createdondate,
+            "profilecomplete": False
         }
         mongo.db.users.insert_one(register)
 
-        # put user in session cookie and go to activity page
+        # put user in session cookie and go to complete profile page
         session["user"] = request.form.get("email").lower()
-        return redirect(url_for("show_map"))
+        flash("Please fill out the form below so we can provide you with an optimal application experience.", "completeprofilemessage")
+        return redirect(url_for("edit_profile", user=session["user"]))
     return render_template("register.html", isRegister=True)
 
 
@@ -169,15 +172,99 @@ def permanently_delete_run(run_id):
 
 @app.route("/profile/<user>", methods=["GET", "POST"])
 def profile(user):
-    # get the session user by registered email address
-    # user = mongo.db.users.find_one(
-    #   {"email": session["user"]})["email"]
-
+    genders = mongo.db.genders.find()    
     user = mongo.db.users.find_one({"email": user})
-
+            
     return render_template("profile.html", 
-                            user=user,
-                            isProfile=True)
+                            user=user, genders=genders, isProfileComplete=True)
+
+
+def determine_user_level(age, besttime):
+    if int(age) < 30:
+        if besttime > datetime.datetime.strptime("01:05:39", "%H:%M:%S"):
+            userlevel = "Beginner"
+        elif besttime < datetime.datetime.strptime("01:05:39", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:54:42", "%H:%M:%S"):
+            userlevel = "Novice"
+        elif besttime < datetime.datetime.strptime("00:54:42", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:47:01", "%H:%M:%S"):
+            userlevel = "Intermediate"
+        elif besttime < datetime.datetime.strptime("00:47:01", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:41:20", "%H:%M:%S"):
+            userlevel = "Advanced"
+        else:
+            userlevel = "Elite"
+    elif int(age) > 30 & int(age) < 35:
+        if besttime > datetime.datetime.strptime("01:06:15", "%H:%M:%S"):
+            userlevel = "Beginner"
+        elif besttime < datetime.datetime.strptime("01:06:15", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:55:13", "%H:%M:%S"):
+            userlevel = "Novice"
+        elif besttime < datetime.datetime.strptime("00:55:13", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:47:27", "%H:%M:%S"):
+            userlevel = "Intermediate"
+        elif besttime < datetime.datetime.strptime("00:47:27", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:41:42", "%H:%M:%S"):
+            userlevel = "Advanced"
+        else:
+            userlevel = "Elite"
+    elif int(age) > 35 & int(age) < 40:
+        if besttime > datetime.datetime.strptime("01:08:08", "%H:%M:%S"):
+            userlevel = "Beginner"
+        elif besttime < datetime.datetime.strptime("01:08:08", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:56:46", "%H:%M:%S"):
+            userlevel = "Novice"
+        elif besttime < datetime.datetime.strptime("00:56:46", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:48:48", "%H:%M:%S"):
+            userlevel = "Intermediate"
+        elif besttime < datetime.datetime.strptime("00:48:48", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:42:53", "%H:%M:%S"):
+            userlevel = "Advanced"
+        else:
+            userlevel = "Elite"
+    else:
+        if besttime > datetime.datetime.strptime("01:10:53", "%H:%M:%S"):
+            userlevel = "Beginner"
+        elif besttime < datetime.datetime.strptime("01:10:53", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:59:04", "%H:%M:%S"):
+            userlevel = "Novice"
+        elif besttime < datetime.datetime.strptime("00:59:04", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:50:46", "%H:%M:%S"):
+            userlevel = "Intermediate"
+        elif besttime < datetime.datetime.strptime("00:50:46", "%H:%M:%S") & besttime > datetime.datetime.strptime("00:44:37", "%H:%M:%S"):
+            userlevel = "Advanced"
+        else:
+            userlevel = "Elite"
+    return userlevel
+
+
+@app.route("/edit_profile/<user>", methods=["GET", "POST"])
+def edit_profile(user):
+    if request.method == "POST":
+        formdob = request.form.get("dateofbirth")
+        dob = datetime.datetime.strptime(formdob, "%Y-%m-%d").date()
+        print("date of birth: ", dob)
+        delta = timestamp.date() - dob
+        print("delta: ", delta)
+        age = str(delta / 365.25)
+        print("age: ", age)
+        formbesttime = request.form.get("besttime")
+        besttime = datetime.datetime.strptime(formbesttime, "%H:%M:%S")
+        print("besttime: ", besttime)
+        # print(strformbesttime)
+        # besttime = datetime.datetime.strptime(formbesttime, "%H:%M:%S").time()
+        # print("Hour: ", besttime.hour)
+        # print("Minute: ", besttime.minute)
+        # print("Second: ", besttime.second)
+        # print(besttime)
+        # userlevel = determine_user_level(age, besttime)
+        completeprofile = {
+            "firstname": request.form.get("firstname"),
+            "lastname": request.form.get("lastname"),
+            "dateofbirth": request.form.get("dateofbirth"),
+            "gender": request.form.get("gender"),
+            "location": request.form.get("location"),
+            "besttime": request.form.get("besttime"),
+            # "userlevel": userlevel,
+            "profilecomplete": True
+        }
+        mongo.db.users.update({"email": user}, {"$set": completeprofile})
+        flash("Profile Succesfulyy Updated.")
+        return redirect(url_for("get_runs"))
+    
+    genders = mongo.db.genders.find()    
+    user = mongo.db.users.find_one({"email": user})
+    return render_template("edit_profile.html", 
+                            user=user, genders=genders, isEditProfile=True)
 
 
 @app.route("/logout")
