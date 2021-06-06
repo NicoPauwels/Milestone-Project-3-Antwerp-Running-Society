27d91726b279ec3e0e54fdc5325656b3026a9a10
NicoPauwels <nico_pauwels@hotmail.com>
2021-06-06 16:57:08 +0200

    removed redundant js

--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,6 @@
-// leaflet map
-
-// var mymap = L.map('mapid').setView([51.208, 4.425], 13);
-
-// mymap.zoomControl.setPosition('topright');
-
-//L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//    maxZoom: 18,
-//    id: 'mapbox/dark-v10',
-//    tileSize: 512,
-//    zoomOffset: -1,
-//    accessToken: 'pk.eyJ1IjoibmNwd2xzIiwiYSI6ImNrbmx1YTR1ejBha2gybmt4cDVrcDN6OWgifQ.wmt7OvrQdw7yqsgvK5h_xg'
-//}).addTo(mymap);
-
-// check whether inputdate is bigger than now
-
-function checkDate() {
-  var inputdate = new Date(document.getElementById('date').value);
-  var today = new Date();
-  if(inputdate.getDate() < today.getDate()) {
-    console.log("Input date is in the past.")
-  }
-}
-
 // add run form distance slider
 
-function updateTextInput(val) {
+function updateDistanceInput(val) {
   document.getElementById('distance').value=val + " km";
   document.getElementById('distance').style.color = "#00BF00"; 
-}
-
-// show and hide upcomingruns
-
-function showUpcomingRunsPane() {
-  upcomingrunspane = document.getElementById("upcomingruns");
-
 }
